b2c8710f9bc8c4fd437d14f9894986a6156594fa
Scooter)Ok <scooterok@gmail.com>
2017-12-01 17:08:18 +0200

    animation

--- js/main.js
+++ js/main.js
@@ -10,7 +10,8 @@
 		data : {
 			animDone : {
 				mainTabs : false,
-				tab1Use : false
+				tab1Use : false,
+				tab1Photo : false
 			}
 		},
 		init : function(){						
@@ -28,7 +29,7 @@
 		events : function(){
 			$(window).scroll(function(e) {
 				app.scroller();
-				if($(document).scrollTop() > 0){
+				if($(document).scrollTop() > ($('.main__tabs').offset().top + $('.main__tabs').height())){
 					TweenMax.to('.header__calk_but', 0.35, {css : {width : '224px', marginLeft : '81px'}, ease: Power2.easeOut});
 				}else{
 					TweenMax.to('.header__calk_but', 0.35, {css : {width : '0', marginLeft : '0'}, ease: Power2.easeOut});
@@ -72,6 +73,19 @@
 			},
 			tab1Use : function(){
 				TweenMax.set('.tab-1__use-left, .tab-1__use-right', {opacity : 1});
+				TweenMax.fromTo('.tab-1__use-left h2 span', 1.5, {opacity : 0, x : -100}, {opacity : 1, x : 0});
+				TweenMax.fromTo('.tab-1__use-left h2 b', 1.5, {opacity : 0, x : 100}, {opacity : 1, x : 0});
+				TweenMax.fromTo('.tab-1__descr', 1.5, {opacity : 0, y : 50}, {opacity : 1, y : 0, delay : 0});
+				TweenMax.fromTo('.tab-1__use-right', 2, {opacity : 0, y : -70}, {opacity : 1, y : 0});
+			},
+			tab1Photo : function(){				
+				TweenMax.set('.tab-1__photo img, .tab-1__photo_circle', {opacity : 0});
+				TweenMax.set('.tab-1__photo_wings', {scale : 0});
+				TweenMax.set('.tab-1__photo', {opacity : 1});				
+				TweenMax.to('.tab-1__photo img', 2, {opacity : 1});
+				TweenMax.fromTo('.tab-1__photo_circle', 1, {opacity : 0, rotation : -360}, {opacity : 1, rotation : 0, delay : 0.3});
+				TweenMax.staggerTo(['.tab-1__photo_wings.country', '.tab-1__photo_wings.honors', '.tab-1__photo_wings.history', '.tab-1__photo_wings.iso'], 0.3, {scale : 1, delay : 1, ease: Back.easeOut.config(1.8)}, 0.3);
+				
 			}
 		},
 		scroller : function(){
@@ -80,9 +94,13 @@
 				app.data.animDone.mainTabs = true;
 				app.animation.mainTabs();
 			}
-			if(st > ($('.tab-1__use').offset().top - ($(window).height() / 1.5)) && !app.data.animDone.tab1Use){
+			if(st > ($('.tab-1__use').offset().top - ($(window).height() / 2.5)) && !app.data.animDone.tab1Use){
 				app.data.animDone.tab1Use = true;
 				app.animation.tab1Use();	
+			}
+			if(st > ($('.tab-1__photo').offset().top - ($(window).height() / 2.3)) && !app.data.animDone.tab1Photo){
+				app.data.animDone.tab1Photo = true;
+				app.animation.tab1Photo();
 			}
 
 		},
